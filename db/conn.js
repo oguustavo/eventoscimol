8eab1c9544d4d2dc6dd1325620cdd78ffcaead13
Gustavo Soares <gustavosoaresok@gmail.com>
2024-11-26 23:40:12 -0300

    Add mysql2 package and update database connection

--- db/conn.js
+++ db/conn.js
@@ -2,13 +2,14 @@ const {Sequelize} = require('sequelize')
 require('dotenv').config()
 
 const sequelize = new Sequelize(
-    process.env.MYSQL_DATABASE || 'railway',
-    process.env.MYSQL_USER || 'root',
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQL_USER,
     process.env.MYSQL_PASSWORD,
     {
         host: process.env.MYSQL_HOST,
         port: process.env.MYSQL_PORT,
         dialect: 'mysql',
+        dialectModule: require('mysql2'),
         dialectOptions: {
             ssl: {
                 rejectUnauthorized: false
