fa05dc3bbe155817e21d57e30ca9db14b2520da2
Gustavo Soares <gustavosoaresok@gmail.com>
2024-11-26 23:38:29 -0300

    d

--- db/conn.js
+++ db/conn.js
@@ -2,8 +2,8 @@ const {Sequelize} = require('sequelize')
 require('dotenv').config()
 
 const sequelize = new Sequelize(
-    process.env.MYSQL_DATABASE,
-    process.env.MYSQL_USER,
+    process.env.MYSQL_DATABASE || 'railway',
+    process.env.MYSQL_USER || 'root',
     process.env.MYSQL_PASSWORD,
     {
         host: process.env.MYSQL_HOST,
@@ -11,25 +11,10 @@ const sequelize = new Sequelize(
         dialect: 'mysql',
         dialectOptions: {
             ssl: {
-                require: true,
                 rejectUnauthorized: false
             }
-        },
-        logging: console.log
+        }
     }
 )
-
-// Teste de conexão mais detalhado
-sequelize.authenticate()
-    .then(() => {
-        console.log('Conexão estabelecida com sucesso.');
-        return sequelize.query('SHOW TABLES');
-    })
-    .then(([results]) => {
-        console.log('Tabelas no banco:', results);
-    })
-    .catch(err => {
-        console.error('Erro detalhado ao conectar:', err);
-    });
 
 module.exports = sequelize
